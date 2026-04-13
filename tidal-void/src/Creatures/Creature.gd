2499de2d844616adf03a734ee5a76228c536649f
Rndname9 <j9lrob@gmail.com>
2026-04-13 13:35:45 -0700

    New Jeremy shader fix attempt

--- tidal-void/src/Creatures/Creature.gd
+++ tidal-void/src/Creatures/Creature.gd
@@ -9,7 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var screen_pos = game_manager.player.get_global_transform_with_canvas().origin
-	var viewport_size = get_viewport().get_visible_rect().size
-	var player_uv = screen_pos / viewport_size
-	material.set_shader_parameter("player_screen_uv", player_uv);
+	pass
