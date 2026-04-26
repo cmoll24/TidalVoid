aede60030f7a0be1461c1c9424032b3beabde05f
IDrinksWaters <idrinkcoldwater@gmail.com>
2026-04-26 16:29:06 -0700

    upgrade_store_wip2

--- tidal-void/src/upgrade_inventory_ui/upgrade_store.gd
+++ tidal-void/src/upgrade_inventory_ui/upgrade_store.gd
@@ -2,7 +2,6 @@ extends Control
 
 @onready var store_grid = $StoreGrid
 @onready var back_button = $BackButton
-
 @export var available_upgrades: Array[upgrade_store_item] = []
 
 var store_slot_scene = preload("res://src/upgrade_inventory_ui/upgrade_store_slot.tscn")
@@ -12,10 +11,15 @@ func _ready():
 	populate_store()
 
 func populate_store():
+	print("populate_store called")
+	print("store_grid: ", store_grid)
+	print("upgrade count: ", available_upgrades.size())
+	
 	for upgrade in available_upgrades:
 		var slot = store_slot_scene.instantiate()
 		store_grid.add_child(slot)
 		slot.set_store_item(upgrade)
+		print("slot added, child count: ", store_grid.get_child_count())
 
 func _on_back_button_pressed():
 	visible = false
--- tidal-void/src/upgrade_inventory_ui/upgrade_store_slot.gd
+++ tidal-void/src/upgrade_inventory_ui/upgrade_store_slot.gd
@@ -17,13 +17,11 @@ func set_store_item(new_store_item: upgrade_store_item):
 func _on_buy_button_pressed() -> void:
 	if store_item == null:
 		return
-	
-
+		
 	if GV.has_item(store_item.cost_item_name, store_item.cost_quantity):
 		GV.remove_item_by_name(store_item.cost_item_name, store_item.cost_quantity)
 		store_item.upgrade.apply_effect(GV.player_node)
-		print("Purchased: ", store_item.upgrade_name)
 		insufficient_label.visible = false
+		
 	else:
 		insufficient_label.visible = true
-		print("Not enough resources!")
