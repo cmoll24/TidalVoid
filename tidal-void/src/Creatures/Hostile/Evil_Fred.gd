51e97c12d6692c3e6d8618afc9915dead09be210
Rndname9 <j9lrob@gmail.com>
2026-05-04 19:16:01 -0700

    Fixed evil fred

--- tidal-void/src/Creatures/Hostile/Evil_Fred.gd
+++ tidal-void/src/Creatures/Hostile/Evil_Fred.gd
@@ -6,12 +6,12 @@ class_name EvilFred
 func _ready() -> void:
 	super._ready()
 	#set the vision bitmask( use the | operator to add more)
-	v_types = 1 << VisionSource.v_source_type.sPrey | 1 << VisionSource.v_source_type.mPrey
+	v_types = (1 << VisionSource.v_source_type.sPrey) | (1 << VisionSource.v_source_type.mPrey)
 	
 func on_collide_with_other_drift_body(other : DriftBody) -> void:
 	super.on_collide_with_other_drift_body(other);
 	var vs : VisionSource = other.get_node_or_null("VisionSource")
-	if(vs && vs.v_type & v_types):
+	if(vs && (1 << vs.v_type) & v_types):
 		if(vs == primary_v_source):
 			primary_v_source = null;
 		other.die()
