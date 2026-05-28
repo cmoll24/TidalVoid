7dca4da06ba6cb90e613893373d6a1a011debea6
Liam McCabe <185857937+Icicle333@users.noreply.github.com>
2026-05-28 16:43:46 -0700

    Shield Ability
    Player doesn't take damage for 3 seconds when activated, only has 5 uses until you need to return to ship

--- tidal-void/src/player/Abilities/Ability_Classes/shield_ability.gd
+++ tidal-void/src/player/Abilities/Ability_Classes/shield_ability.gd
@@ -1,11 +1,21 @@
 extends PlayerAbility
 
+@export var shield_duration : float = 3.0
+var shield_sprite_scene
+
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func init_ability() -> void:
+	shield_sprite_scene = preload("res://src/player/Abilities/Ability_Classes/shield_sprite.tscn")
+func trigger_ability(player : PlayerPawn) -> bool:
+	if(!super.trigger_ability(player)):
+		return false
+	
+	var shield_reference : ShieldSprite = shield_sprite_scene.instantiate()
+	shield_reference.lifetime = shield_duration
+	player.add_child(shield_reference)
+	var health_component : HealthComponent = player.get_node_or_null("HealthComponent")
+	if health_component != null:
+		health_component.grace_period_time = shield_duration
+	return true
+	
--- tidal-void/src/player/Abilities/toolbar.gd
+++ tidal-void/src/player/Abilities/toolbar.gd
@@ -21,6 +21,7 @@ func _ready() -> void:
 	add_ability_to_slot(0, typeof(Player), "res://src/player/Abilities/Ability_Classes/propulsion_ability.tscn")
 	add_ability_to_slot(1, typeof(Player), "res://src/player/Abilities/Ability_Classes/teleport_ability.tscn")
 	add_ability_to_slot(2, typeof(Player), "res://src/player/Abilities/Ability_Classes/boost_ability.tscn")
+	add_ability_to_slot(3, typeof(Player), "res://src/player/Abilities/Ability_Classes/shield_ability.tscn")
 func add_ability_to_slot(index : int, type, ability_path : String):
 	if index < abilities_slot_max and index >= 0:
 		#delete the old ability if there is one
