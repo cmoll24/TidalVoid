8d9421ce8c05a27a2516ca13836839290c251675
Rndname9 <j9lrob@gmail.com>
2026-06-01 11:26:15 -0700

    Updated evil fred asset

--- tidal-void/src/Creatures/Hostile/Evil_Fred.gd
+++ tidal-void/src/Creatures/Hostile/Evil_Fred.gd
@@ -4,12 +4,27 @@ class_name EvilFred
 @export var bite_damage : float = 90
 @export var bite_knockback : float = 20
 
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
 	#set the vision bitmask( use the | operator to add more)
 	v_types = (1 << VisionSource.v_source_type.sPrey) | (1 << VisionSource.v_source_type.mPrey)
+	
+	
+func _physics_process(delta: float) -> void:
+	if(primary_v_source):
+		#custom rotation if we have a primary v source
+		b_rotation_to_gravity = false
+		
+		target_rotation = (primary_v_source.parent.global_position - global_position).angle() - (0.8*PI)
+	else:
+		b_rotation_to_gravity = true
+	
+	#call super after disabled or enabling rotation
+	super._physics_process(delta);
 	
 func on_collide_with_other_drift_body(other : DriftBody) -> void:
 	super.on_collide_with_other_drift_body(other);
@@ -25,3 +40,14 @@ func on_collide_with_other_drift_body(other : DriftBody) -> void:
 				hc.take_damage(bite_damage,HealthComponent.e_dmg_types.physical,self,self)
 				#knockback damage
 				hc.take_damage(bite_knockback,HealthComponent.e_dmg_types.knockback,self,self)
+
+func lunge(dist : float):
+	super.lunge(dist);
+	#set the sprite to open the mouth
+	sprite.frame = 1
+	#close the mouth later
+	get_tree().create_timer(lunge_cldwn_time - 0.5).timeout.connect(set_sprite_closed)	
+
+
+func set_sprite_closed():
+	sprite.frame = 0
