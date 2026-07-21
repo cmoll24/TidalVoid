743f8e43c48485819c87e60653040d82b66736d3
Liam McCabe <185857937+Icicle333@users.noreply.github.com>
2026-07-20 20:26:12 -0700

    Added ability limit to jump boost and created the scens/scripts needed for invisibility

--- tidal-void/src/player/Abilities/Ability_Classes/invisibility_ability.gd
+++ tidal-void/src/player/Abilities/Ability_Classes/invisibility_ability.gd
@@ -1,12 +1,7 @@
 extends PlayerAbility
 
-@export var jump_power : int = 100
 
 func trigger_ability(player : PlayerPawn) -> bool:
-	#update uses and return false if uses 0 or below
 	if(!super.trigger_ability(player)):
 		return false
-	player.jump_boost_activated(true)
-	
-	
 	return true
