a4a6c894aa47efb8cf6711d1823c9119a78e4582
Liam McCabe <185857937+Icicle333@users.noreply.github.com>
2026-07-20 21:54:20 -0700

    Invisibiltiy timer turning out to be harder than I thought, will continue if I can tomorrow

--- tidal-void/src/player/Abilities/Ability_Classes/invisibility_ability.gd
+++ tidal-void/src/player/Abilities/Ability_Classes/invisibility_ability.gd
@@ -1,8 +1,19 @@
 extends PlayerAbility
 
-@onready var player_sprite : Sprite2D = $PlayerSprite
-
+var timer : Timer
+@export var invisibility_duration = 10
+func _ready() -> void:
+	timer = Timer.new()
+	timer.wait_time = 10.0
+	timer.one_shot = true
+	add_child(timer)
+	
 func trigger_ability(player : PlayerPawn) -> bool:
-	print("test")
 	player.visible = false
+	
+	
 	return true
+	
+func _on_timer_timeout() -> void:
+	print("done")
+	pass # Replace with function body.
